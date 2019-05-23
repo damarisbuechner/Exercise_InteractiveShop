69810706da7f7435d5eb6d4f8396b1ec03d87fc9
damarisbuechner <damaris.buechner@zhdk.ch>
2019-05-23 15:04:40 +0200

    removeClass (choosen productimage)

--- InteractiveShop_JS.js
+++ InteractiveShop_JS.js
@@ -1,6 +1,14 @@
-$(document).ready(function)(){
+$(document).ready(function(){
+  // console.log('halo');
 
-
+$(".mini-productimage").click(function() {
+  // console.log('click');
+  // var boxes = $(".image-box");
+  // console.log(boxes);
+  $(".big-productimage").removeClass("choosen-productimage")
+  // .addClass("choosen-productimage")
+  find(".mini-productimage")
+});
 
 
 
