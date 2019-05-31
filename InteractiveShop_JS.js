3367b8bb88059e98c9505256a1d807b322d977f3
damarisbuechner <damaris.buechner@zhdk.ch>
2019-05-31 13:30:53 +0200

    midstate textoverview box

--- InteractiveShop_JS.js
+++ InteractiveShop_JS.js
@@ -1,25 +1,6 @@
 $(document).ready(function(){
  console.log('hallo');
 
-// $(".mini-productimage").click(function() {
-// console.log('click');
-// $(this).css("stroke", "red");
-//   // var boxes = $(".image-box");
-//   // console.log(boxes);
-//   $(".big-productimage").toggleClass("choosen-productimage")
-//   // .addClass("choosen-productimage")
-// });
-
-// $(".mini-productimage").click(function(event) {
-//    console.log('change that image! 👻');
-//    // console.log('event.target: ', event.target);
-//
-//
-//    var index = $(this);
-//    console.log("index" + index);
-//
-//    $(".big-productimage").removeClass("choosen-productimage");
-
 var $miniimages = $(".mini-productimage");
 console.log("$miniimages", $miniimages);
 
